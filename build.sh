e2629b2ba84c65cdf878b329e30be31db725f2fa
Bixente Grandjean <57395127+BixGra@users.noreply.github.com>
2024-11-20 17:26:57 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,7 @@ docker build . -t app1 -f app1.Dockerfile
 echo "docker build . -t app2 -f app2.Dockerfile"
 
 docker build . -t app2 -f app2.Dockerfile
+
+echo "docker compose up -d --remove-orphans"
+
+docker compose up -d --remove-orphans
